167f54e575632414711644898087275e78c6e7ac
Vineet kumar <vineet123koli@gmail.com>
2026-02-04 23:22:59 +0530

    Delete OOPS_Steganography_project directory

--- OOPS_Steganography_project/main/encryption/AESAlgorithm.java
+++
@@ -1,4 +0,0 @@
-package main.encryption;
-
-public class AESAlgorithm {
-}
--- OOPS_Steganography_project/main/encryption/AbstractCrypto.java
+++
@@ -1,9 +0,0 @@
-package main.encryption;
-
-public abstract class AbstractCrypto {
-    protected static final String ALGORITHM = "AES";
-    protected static final String FILE_PATH = "src/main/input_output/encrypted_message.txt";
-
-    // Abstract method to be implemented by Encryption and Decryption classes
-    public abstract String process(String input, String passkey) throws Exception;
-}
